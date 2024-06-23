0b7c317e05255b768831c1f7c310126d1ed653fd
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-06-23 11:23:37 -0500

    Explicit imports, implement powershell fallback for missing wmic

--- AMDVanillaPatches.py
+++ AMDVanillaPatches.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys, tempfile, shutil, binascii, plistlib, subprocess, platform
-from Scripts import *
+from Scripts import utils, downloader, plist
 
 class AMDPatch:
     def __init__(self):
@@ -120,7 +120,11 @@ class AMDPatch:
             if _platform == "darwin":
                 return int(subprocess.check_output(["sysctl", "-a", "machdep.cpu.core_count"]).decode().split(":")[1].strip())
             elif _platform == "windows":
-                return int(subprocess.check_output(["wmic", "cpu", "get", "NumberOfCores"]).decode().split("\n")[1].strip())
+                try:
+                    return int(subprocess.check_output(["wmic", "cpu", "get", "NumberOfCores"]).decode().split("\n")[1].strip())
+                except:
+                    data = subprocess.check_output(["powershell","-c","$p=Get-ComputerInfo -Property CsProcessors;$p.CsProcessors"]).decode().split("\n")
+                    return next((int(x.split(":")[-1].strip()) for x in data if x.startswith("NumberOfCores")),-1)
             elif _platform == "linux":
                 data = subprocess.check_output(["cat", "/proc/cpuinfo"]).decode().split("\n")
                 for line in data:
